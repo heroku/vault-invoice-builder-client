9242e0555769a5dfd491c6b99dee94ea2f5f1144
Rahsun McAfee <rahsun@rahsunmcafee.com>
2015-06-25 17:08:47 -0400

    Gem and Security Update

--- lib/vault-invoice-builder-client/version.rb
+++ lib/vault-invoice-builder-client/version.rb
@@ -2,7 +2,7 @@ module Vault
   module InvoiceBuilder
     class Client
       # The `Vault::InvoiceBuilder::Client` gem version.
-      VERSION = '0.0.3'
+      VERSION = '0.0.4'
     end
   end
 end
